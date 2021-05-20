ee2ef982de32254a91b2879088a6d9ffa377c067
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-05-20 12:26:51 +0530

    default path and customer path

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use('/',(resp,req)=>{
+app.use('/',(res,req)=>{
+    console.log("Default Path")
+    res.send("Wc Default Path")
 })
+
+app.use('/customer',(res, req)=>{
+    console.log("Customer Path")
+    res.send("Wc Customer Path")
+})
+
 
 app.listen(port,()=>{
     console.log("Server Started...")
