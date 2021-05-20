06002349b3d27936d2b4264f616e9547c1d200d4
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-05-20 12:44:19 +0530

    item path added

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use('/',(res,req)=>{
-    console.log("Default Path")
-    res.send("Wc Default Path")
+// app.use('/',(req,res)=>{
+//     console.log("Default Path")
+//     res.send("Wc Default Path")
+// })
+
+app.use('/customer',(req, res)=>{
+    console.log("Customer Path")
+    res.send("Response Customer Path")
 })
 
-app.use('/customer',(res, req)=>{
-    console.log("Customer Path")
-    res.send("Wc Customer Path")
+app.use('/item',(req, res)=>{
+    console.log("Item Path")
+    res.send("Response Item Path")
 })
+
 
 
 app.listen(port,()=>{
